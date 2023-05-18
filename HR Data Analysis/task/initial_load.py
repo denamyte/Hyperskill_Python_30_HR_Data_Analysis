1002160afcebfd0e2bb6cf41c38b302b10d200e8
denys <denamyte@gmail.com>
2023-05-18 23:15:30 +0300

    2023-05-18: Stage 1/5: in progress 1

---
+++ HR Data Analysis/task/initial_load.py
@@ -0,0 +1,29 @@
+import requests
+import os
+
+
+def load():
+    if not os.path.exists('../Data'):
+        os.mkdir('../Data')
+
+    # Download data if it is unavailable.
+    if ('A_office_data.xml' not in os.listdir('../Data') and
+        'B_office_data.xml' not in os.listdir('../Data') and
+        'hr_data.xml' not in os.listdir('../Data')):
+        print('A_office_data loading.')
+        url = "https://www.dropbox.com/s/jpeknyzx57c4jb2/A_office_data.xml?dl=1"
+        r = requests.get(url, allow_redirects=True)
+        open('../Data/A_office_data.xml', 'wb').write(r.content)
+        print('Loaded.')
+
+        print('B_office_data loading.')
+        url = "https://www.dropbox.com/s/hea0tbhir64u9t5/B_office_data.xml?dl=1"
+        r = requests.get(url, allow_redirects=True)
+        open('../Data/B_office_data.xml', 'wb').write(r.content)
+        print('Loaded.')
+
+        print('hr_data loading.')
+        url = "https://www.dropbox.com/s/u6jzqqg1byajy0s/hr_data.xml?dl=1"
+        r = requests.get(url, allow_redirects=True)
+        open('../Data/hr_data.xml', 'wb').write(r.content)
+        print('Loaded.')
